dd71bef58f3814fdc95403243a1324cb8cf011df
Logan Olzenak <logan.olzenak@gmail.com>
2018-03-01 17:06:51 -0600

    set

--- server/routers/employeeRouter.js
+++ server/routers/employeeRouter.js
@@ -3,4 +3,34 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 
+
+
+
+// router.post('/', (request, response)=> {
+//     let newEmployee = new Employee (request.body);
+//     console.log('employee added');
+//     newEmployee.save(( ) => {
+//         if(error){
+//             console.log('error in posting employee', error);
+//             response.sendStatus(500);            
+//         }else {
+//             response.sendStatus(201);
+//         }
+//     })
+    
+// });
+
+// router.get('/'), (request, response) => {
+
+// }
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
