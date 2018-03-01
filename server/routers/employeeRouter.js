f715bc49ac617d65470ed5ec53f2e8a8bddaf4c6
Logan Olzenak <logan.olzenak@gmail.com>
2018-03-01 17:22:09 -0600

    CRUD is set

--- server/routers/employeeRouter.js
+++ server/routers/employeeRouter.js
@@ -35,43 +35,53 @@ router.get('/', (request, response) => {
   })
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// router.post('/', (request, response)=> {
-//     let newEmployee = new Employee (request.body);
-//     console.log('employee added');
-//     newEmployee.save(( ) => {
-//         if(error){
-//             console.log('error in posting employee', error);
-//             response.sendStatus(500);            
-//         }else {
-//             response.sendStatus(201);
-//         }
-//     })
+router.post('/', (request, response)=> {
+    let newEmployee = new Employee (request.body);
+    console.log('employee added');
+    newEmployee.save((error, addedEmployee ) => {
+        if(error){
+            console.log('error in posting employee', error);
+            response.sendStatus(500);            
+        }else {
+            response.sendStatus(201);
+        }
+    })
     
-// });
+});
 
-// router.get('/'), (request, response) => {
+router.put('/:id', (request, response)=> {
+    let id = request.params.id;
+    let employeeToUpdate = request.body;
+    console.log('employeeToUpdate');
+    employee.findByIdAndUpdate(
+        {"id": id},
+        {$set: employeeToUpdate},
+        (error, updateEmployee) =>{
+            if(error){
+                console.log('error on update employee', error );
+                response.sendStatus(500);
+            } else {
+                response.sendStatus(200);
+            }
+        }
+    )
+    
+})
 
-// }
-
-
-
-
-
-
+router.delete('/id:', (request, response)=> {
+    let id = request.params.id;
+    employee.findIdAndRemove(
+        {"id": id},
+        (error , deletedEmployee) => {
+            if (error){
+                console.log('error in delete');
+                response.sendStatus(500);
+            }else{
+                response.sendStatus(200);
+            }
+        }
+    )
+})
 
 
 
