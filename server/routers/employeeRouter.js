e9953d50b72695f13b01f151a2b885f01ed0fb2e
Michael Gregoire <mdgregoire@gmail.com>
2018-03-01 17:04:17 -0600

    employee schema made

--- server/routers/employeeRouter.js
+++ server/routers/employeeRouter.js
@@ -2,5 +2,48 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
+const EmployeeSchema = new mongoose.Schema(
+  {
+    name: {type: String, required: true},
+    salary: {type: Number},
+    title: {type: String, required: true},
+    hiredate: {type: Date, required: true}
+  }
+);//end employee schema
+
+const Employee = mongoose.model('Employee', EmployeeSchema, 'employees');
+
+let testEmployee = new Employee(
+  {
+    name: 'Test Employee',
+    salary: 20000,
+    title: 'Job Title',
+    hiredate: new Date('2018-02-01')
+  }
+);//end test employee build
+
+testEmployee.save();
+
+router.get('/', (request, response) => {
+  Game.find({}, (error, foundGames) => {
+    if (error){
+      console.log('error on find games:', error);
+      response.sendStatus(500);
+    } else {
+      response.send(foundGames);
+    }
+  })
+});
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
